443b99193fc2b5257455e7cdfd32f2d98c5f7c39
Stephen Matthews <stephen.matthews@hey.com>
2020-10-06 11:25:48 +0100

    refactor: update users and kids schema

--- server/model/kids.js
+++ server/model/kids.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 const kidSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  userId: { type: String, required: true },
+  parentId: { type: String, required: true },
+  allowanceAmount: { type: Number, required: true },
   allowanceFrequency: { type: String, required: true },
   allowanceDay: { type: Number, required: true },
-  allowanceAmount: { type: Number, required: true },
+  createdAt: { type: Date, required: true, default: Date.now() },
+  updatedAt: { type: Date, required: true, default: Date.now() },
 });
 
 const Kid = mongoose.model('kid', kidSchema);
--- server/model/users.js
+++ server/model/users.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  allowanceFrequency: { type: String, required: true },
-  allowanceDay: { type: Number, required: true },
-  allowanceAmount: { type: Number, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  isKid: { type: Boolean, required: true },
+  createdAt: { type: Date, required: true, default: Date.now() },
+  updatedAt: { type: Date, required: true, default: Date.now() },
 });
 
 const User = mongoose.model('user', userSchema);
